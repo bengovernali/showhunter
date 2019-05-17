f896e71c47f982abc86681da7212212f45f6b641
Pierson Scarborough <piersonscarborough@Piersons-MacBook-Pro.local>
2019-05-17 13:26:25 -0400

    Working MVP

--- scripts/getSpotify.js
+++ scripts/getSpotify.js
@@ -1,7 +1,7 @@
 function getSpotify(URL) {
     return fetch(URL, {
         headers: {
-            'Authorization': 'Authorization: Bearer BQDEjd3tHurIvuz38mro2ljT5ZB0BJTB2r8jtGDbyj3VOYcXOFBvt0iLgCm8Mxd_eae39VA2UC_pJsfys-xJ_yCvoPh4bpnEevIpQ1jJnGc6Lc_84RDxW8OR8DTzxSs3-ePl6Timy_JUtR0uOCs65L3v'
+            'Authorization': 'Authorization: Bearer BQByhAsfK5BKogLQer0Rem5eLKZPixn61fsT8Au-eUFgkf_ciQgkNsVXnEmozw2akp5NiOjmh4-4qEoaPfxcXv8pOAnj3TdRJ92y331Om5jHn6wEjm_dKUptj4uCwM9yzd0zEC5lqZBb5Vzdew'
         }})
             .then(function(response) {
             return response.json();
