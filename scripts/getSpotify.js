a77196615698b69c877d4be3b98112b305dab4d2
Ben Governali <bgovern93@gmail.com>
2019-05-21 13:36:20 -0400

    added new oauth token for class demo

--- scripts/getSpotify.js
+++ scripts/getSpotify.js
@@ -1,7 +1,7 @@
 function getSpotify(URL) {
     return fetch(URL, {
         headers: {
-            'Authorization': 'Authorization: Bearer BQBiYU-f4TK8sIJN8SV75TiSwpPFIPvRouqt2EZFKnDkc7jGyBLun_1TChZWN1AybuGfb7GyjtH4625APbI7O97S0ioiSF2HbrKJHvTZn2bUBKRh2OGiiHa7Pe9SRH7jY1dhsXvqK3hKZRgsDJf3nep5'
+            'Authorization': 'Authorization: Bearer BQCWzCUJdQFUw_MytUVwDG4nnfIElvu8WfW1UtIAYfYayMAc3a7KSSLeKgwNY-Towu-nv92qaZiE01dA5QJQ-oMTikSBzXszf1zvtpZuZFBoYENoQKcot7HYEbyJ2U2rbV07wAfBQG2_ci3LdEZN5FT1'
         }})
             .then(function(response) {
             return response.json();
