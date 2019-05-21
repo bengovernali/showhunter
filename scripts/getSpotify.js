350f4ae158c97deb36d6c7a25f720d0242cacc2f
Ben Governali <bgovern93@gmail.com>
2019-05-21 11:08:43 -0400

    added meta tag to html to fix issues with devices ignoring media queries.

--- scripts/getSpotify.js
+++ scripts/getSpotify.js
@@ -1,7 +1,7 @@
 function getSpotify(URL) {
     return fetch(URL, {
         headers: {
-            'Authorization': 'Authorization: Bearer BQDpVH-1ZcSTZyGydA_sGz5kP0ZectzN7E3rGkbFPUN7ANxF3941njpxzpNQNV5cCB765PrWH179Z6GpKp7FG8NYxWmJ0jvfm2D8SQoic3w7JbEQhAciwHjYHADm4Rm7OqjGWaP9v5Rkz-ncEU9VwKX9'
+            'Authorization': 'Authorization: Bearer BQCxNYYIZYYvUWWIK57wCETNiSFXhVneAwS4lXYWu2t4KoXZPEvba5b8QoCBshYuRsN5i45bKVE6CR7sw1re6Q0JDTsqBWRRkumig2ukRJR8yus-yrMFGyfVMpsREQPeRYNqnxNcOi6Sn1ssWLHYghk5'
         }})
             .then(function(response) {
             return response.json();
